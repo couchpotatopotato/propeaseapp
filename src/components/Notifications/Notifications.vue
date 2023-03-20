9d923310132079b6e423ad50a1c341e0be6ba778
shaynagy <shayna.gy@gmail.com>
2023-03-20 17:30:36 +0800

    Add HTML for Notifications.vue

--- src/components/Notifications/Notifications.vue
+++ src/components/Notifications/Notifications.vue
@@ -1,3 +1,14 @@
 <template>
-    
+    <h1 id="Current">Notifications</h1>
+
+    <table id="table">
+        <tr>
+            <th>Address</th>
+            <th>Tenant</th>
+            <th>Payment Due Date</th>
+            <th>Payment Amount</th>
+            <th>Status</th>
+            <th>Actions</th>
+        </tr>
+    </table> <br><br>
 </template>
