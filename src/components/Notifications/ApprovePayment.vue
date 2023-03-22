27019efc33fc7dff85fbf002315000d92a59dd18
shaynagy <shayna.gy@gmail.com>
2023-03-22 15:41:27 +0800

    Added ApprovePayment.vue

---
+++ src/components/Notifications/ApprovePayment.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Approve Payment</h1>
+
+    <div class="container">
+        <table id="payment">
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</template>
