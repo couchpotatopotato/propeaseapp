0fd72845a0ccb8a5504cd8224534469ecdb2377e
shaynagy <shayna.gy@gmail.com>
2023-04-09 23:39:26 +0800

    Edited Dashboard

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -2,30 +2,116 @@
     <div id="dashboard">
         <div id="metric1" class="card">
             <span class="field">Current Overdue</span> <br>
-            <span class="info">5</span>
+            <span class="info">{{ CurrentOverdue }}</span>
         </div>
 
         <div id="metric2" class="card">
             <span class="field">Active Contracts</span> <br>
-            <span class="info">8</span>
+            <span class="info">{{ NumActiveContracts }}</span>
         </div>
 
         <div id="metric3" class="card">
-            <span class="field">Total Contracts</span> <br>
-            <span class="info">10</span>
+            <span class="field">Total Properties</span> <br>
+            <span class="info">{{ TotalProp }}</span>
         </div>
 
         <div id="metric4" class="card">
-            <span class="field">Available Contracts</span> <br>
-            <span class="info">2</span>
+            <span class="field">Available Properties</span> <br>
+            <span class="info">{{ AvailProp }}</span>
         </div>
 
         <div id="metric5" class="card">
             <span class="field">Total Revenue</span> <br>
-            <span class="info">$42 000</span>
+            <span class="info">{{ TotalRevenue }}</span>
         </div>
     </div>
 </template>
+
+<script>
+import firebaseApp from '@/firebase.js';
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+const db = getFirestore(firebaseApp);
+
+export default {
+    data() {
+        return {
+            CurrentOverdue: "",
+            NumActiveContracts: "",
+            TotalProp: "",
+            AvailProp: "",
+            TotalRevenue: "",
+        };
+    },
+
+    async mounted() {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                this.useremail = user.email;
+            } else {
+                // User is signed out
+            }
+        });
+        await this.fetchAndUpdateData(this.useremail);
+    },
+
+    methods: {
+        async fetchAndUpdateData(useremail) {
+            // Collect data from Property
+            const propRef = collection(db, "Property");
+            const propDetails = await getDocs(propRef);
+            let totalProp = 0;
+            propDetails.forEach((prop) => {
+                if (prop.data().OwnerEmail === this.useremail) {
+                    console.log(prop.id, " => ", prop.data());
+                    totalProp += 1;
+                }
+            });
+
+            // Collect data from Contract
+            const contractRef = collection(db, "Contract");
+            const contractDetails = await getDocs(contractRef);
+            let activeContracts = 0;
+            contractDetails.forEach((contract) => {
+                if (contract.data().OwnerEmail === this.useremail) {
+                    console.log(contract.id, " => ", contract.data());
+                    activeContracts += 1;
+                }
+            });
+
+            // Collect data from Payment
+            const paymentRef = collection(db, "Payment");
+            const paymentDetails = await getDocs(paymentRef);
+            let overdue = 0;
+            paymentDetails.forEach((payment) => {
+                if (payment.data().OwnerEmail === this.useremail && payment.data().Status === "Overdue") {
+                    console.log(payment.id, " => ", payment.data());
+                    overdue += 1;
+                }
+            });
+
+            // Collect data from PaymentHistory
+            const payHistRef = collection(db, "PaymentHistory");
+            const payHistDetails = await getDocs(payHistRef);
+            let totalRevenue = 0;
+            payHistDetails.forEach((doc) => {
+                if (doc.data().OwnerEmail === this.useremail) {
+                    console.log(doc.id, " => ", doc.data());
+                    totalRevenue += doc.data().PaymentAmount;
+                }
+            });
+
+            this.CurrentOverdue = overdue;
+            this.NumActiveContracts = activeContracts;
+            this.TotalProp = totalProp;
+            this.AvailProp = totalProp - activeContracts;
+            this.TotalRevenue = totalRevenue;
+        },
+  },
+};
+</script>
 
 <style scoped>
 #dashboard {
